0eab3b65d2003dc93a8cc88944d3bb42b8afd113
1f604 <x1f604@gmail.com>
2023-12-08 12:54:54 +0000

    Update LongestPrefixMuxer to match on hostname. Add parameter to specify whether or not to log. Add parameter to NginxLog

--- web/Nginx_logging.go
+++ web/Nginx_logging.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func Nginx_Log_Received_Request(r *http.Request) {
+func Nginx_Log_Received_Request(caller string, r *http.Request) {
 	// log format is based on default nginx log format
 	/*
 	  log_format combined '$remote_addr - $remote_user [$time_local] '
@@ -18,5 +18,6 @@ func Nginx_Log_Received_Request(r *http.Request) {
 	} else {
 		http_or_https = "HTTPS"
 	}
-	log.Printf("RemoteAddr: %s Method: %s Scheme: %s Protocol: %s Host: %#q URL: %#q RequestURI:%#q ContentLength:%d Referrer:%#q UserAgent:%#q", r.RemoteAddr, r.Method, http_or_https, r.Proto, r.Host, r.URL, r.RequestURI, r.ContentLength, r.Referer(), r.UserAgent())
+	log.Printf("%s: RemoteAddr: %s Method: %s Scheme: %s Protocol: %s Host: %#q URL: %#q RequestURI:%#q ContentLength:%d Referrer:%#q UserAgent:%#q",
+		caller, r.RemoteAddr, r.Method, http_or_https, r.Proto, r.Host, r.URL, r.RequestURI, r.ContentLength, r.Referer(), r.UserAgent())
 }
