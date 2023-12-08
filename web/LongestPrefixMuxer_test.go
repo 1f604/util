81019564bbf23dc44760b7370823ab97e273831e
The That It <10658503+1f604@users.noreply.github.com>
2023-12-08 12:54:54 +0000

    Update LongestPrefixMuxer to match on hostname. Add parameter to specify whether or not to log. Add parameter to NginxLog
    Signed-off-by: 1f604 <x1f604@gmail.com>

--- web/LongestPrefixMuxer_test.go
+++ web/LongestPrefixMuxer_test.go
@@ -1,7 +1,6 @@
 package util_test
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,6 +11,10 @@ import (
 
 func handle_hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!")) //nolint: errcheck // no need to check error here
+}
+
+func handle_hello2(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("yoooo !!!")) //nolint: errcheck // no need to check error here
 }
 
 func handle_hello_exact(w http.ResponseWriter, r *http.Request) {
@@ -34,12 +37,13 @@ func Test_LongestPrefixMuxer(t *testing.T) {
 	t.Parallel()
 
 	mux_entries := []*web.MuxEntry{
-		web.NewMuxEntry(handle_hello_exact, "/hello", util.EXACT_MATCH_HANDLER),
-		web.NewMuxEntry(handle_hello, "/hello", util.LONGEST_PREFIX_HANDLER),
-		web.NewMuxEntry(handle_helloworldagain, "/helloworldagain", util.LONGEST_PREFIX_HANDLER),
-		web.NewMuxEntry(handle_helloworld, "/helloworld", util.LONGEST_PREFIX_HANDLER),
+		web.NewMuxEntry("example.com", handle_hello_exact, "/hello", util.EXACT_MATCH_HANDLER),
+		web.NewMuxEntry("example.com", handle_hello, "/hello", util.LONGEST_PREFIX_HANDLER),
+		web.NewMuxEntry("example.com", handle_helloworldagain, "/helloworldagain", util.LONGEST_PREFIX_HANDLER),
+		web.NewMuxEntry("example.com", handle_helloworld, "/helloworld", util.LONGEST_PREFIX_HANDLER),
+		web.NewMuxEntry("example2.com", handle_hello2, "/hello", util.EXACT_MATCH_HANDLER),
 	}
-	newmuxer := web.NewLongestPrefixRouter(mux_entries, fallback_handler)
+	newmuxer := web.NewLongestPrefixRouter(mux_entries, fallback_handler, false)
 
 	run_test := func(path string, expected_output string) {
 		rr := httptest.NewRecorder()
@@ -52,25 +56,26 @@ func Test_LongestPrefixMuxer(t *testing.T) {
 		// Check the response body is what we expect.
 		expected := expected_output
 		if rr.Body.String() != expected {
-			fmt.Println(len(rr.Body.String()), len(expected))
+			// fmt.Println(len(rr.Body.String()), len(expected))
 			t.Errorf("handler returned unexpected body: got %v want %v",
 				rr.Body.String(), expected)
 		}
 	}
 
-	run_test("/hel.lo/", "MyCustomMuxer says: Invalid URL path.\n")
+	run_test("https://example.com/hel.lo/", "MyCustomMuxer says: Invalid URL path.\n")
 
-	run_test("/hello", "hello exact!")
-	run_test("/hello/", "hello!")
-	run_test("/helloa", "hello!")
-	run_test("/helloworl", "hello!")
+	run_test("https://example.com/hello", "hello exact!")
+	run_test("https://example2.com/hello", "yoooo !!!")
+	run_test("https://example.com/hello/", "hello!")
+	run_test("https://example.com/helloa", "hello!")
+	run_test("https://example.com/helloworl", "hello!")
 
-	run_test("/helloworld", "hello world!")
-	run_test("/helloworlda", "hello world!")
-	run_test("/helloworldagai", "hello world!")
+	run_test("https://example.com/helloworld", "hello world!")
+	run_test("https://example.com/helloworlda", "hello world!")
+	run_test("https://example.com/helloworldagai", "hello world!")
 
-	run_test("/helloworldagain", "hello world again!")
-	run_test("/helloworldagaina", "hello world again!")
+	run_test("https://example.com/helloworldagain", "hello world again!")
+	run_test("https://example.com/helloworldagaina", "hello world again!")
 
-	run_test("/hell", "fallback!")
+	run_test("https://example.com/hell", "fallback!")
 }
