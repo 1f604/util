f805846e3578802032a8617185241e98a48c9530
The That It <10658503+1f604@users.noreply.github.com>
2023-12-17 18:20:21 +0000

    Fix bug introduced in last commit
    Signed-off-by: 1f604 <x1f604@gmail.com>

--- web_types/posix_filename_validator.go
+++ web_types/posix_filename_validator.go
@@ -5,17 +5,17 @@ import (
 	"regexp"
 )
 
-var g_posix_alnum_char = `[0-9a-zA-Z]`
-var g_posix_alnum_seq = `[0-9a-zA-Z]+`
-var g_posix_alnum_or_sym_seq = `[0-9a-zA-Z_ ,'.-]+` // don't need to escape dash (-) if it's last character inside square brackets
-var g_posix_regex_str = `^(` + g_posix_alnum_seq + `|` + g_posix_alnum_char + g_posix_alnum_seq + g_posix_alnum_char + `)$`
-var g_validationRegex = regexp.MustCompile(g_posix_regex_str)
+var g_posix_filename_alnum_char = `[0-9a-zA-Z]`
+var g_posix_filename_alnum_seq = `[0-9a-zA-Z]+`
+var g_posix_filename_alnum_or_sym_seq = `[0-9a-zA-Z_ ,'.-]+` // don't need to escape dash (-) if it's last character inside square brackets
+var g_posix_filename_regex_str = `^(` + g_posix_filename_alnum_seq + `|` + g_posix_filename_alnum_char + g_posix_filename_alnum_or_sym_seq + g_posix_filename_alnum_char + `)$`
+var g_posix_filename_validationRegex = regexp.MustCompile(g_posix_filename_regex_str)
 
 // Checks that the file name only contains numbers, letters, underscore, dot, and dash.
 func Posix_filename_validator(filename string) error {
 	// check regex
-	if !g_validationRegex.MatchString(filename) {
-		return fmt.Errorf("Error: filename \"%s\" does not match filename validation regex %s", filename, g_validationRegex)
+	if !g_posix_filename_validationRegex.MatchString(filename) {
+		return fmt.Errorf("Error: filename \"%s\" does not match filename validation regex %s", filename, g_posix_filename_validationRegex)
 	}
 
 	return nil
